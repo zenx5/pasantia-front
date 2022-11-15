1accdaf928b2b9807ba77ff62f05f2d2856d73aa
Octavio Martinez <omartinez1618@gmail.com>
2022-11-14 21:05:44 -0400

    lab added

--- src/routes/Sell.jsx
+++ src/routes/Sell.jsx
@@ -1,29 +1,35 @@
 import { useState, useEffect } from 'react';
 import {
     Chart as ChartJS,
+    ArcElement,
     CategoryScale,
     LinearScale,
+    BarElement,
     PointElement,
     LineElement,
     Title,
+    Filler,
     Tooltip,
     Legend,
 } from 'chart.js';
-import { Line } from 'react-chartjs-2';
+import { Chart, Line } from 'react-chartjs-2';
 
 
 export default function Sell( props ) {
     ChartJS.register(
+        ArcElement,
         CategoryScale,
         LinearScale,
+        BarElement,
         PointElement,
         LineElement,
         Title,
+        Filler,
         Tooltip,
         Legend
     );   
     
-    const { labels } = props
+    const { type, labels, dataVisitas, dataLogeados, dataCompras } = props
     
 
     const options = {
@@ -39,34 +45,63 @@ export default function Sell( props ) {
         },
     };
 
-    const dataVisitas = labels.map(() => (Math.random()*1000) )
-    const dataLogeados = dataVisitas.map( dato => Math.random()*dato )
-    const dataCompras = dataLogeados.map( dato => Math.random()*dato/2 )
-    const data = {
+    let data = {}
+    const $type = type.split('.').at(0)
+    const mode = type.split('.').at(1)
+    if( $type === 'pie' ){
+      data = {
         labels,
         datasets: [
           {
             label: 'Visitas',
-            data: dataVisitas,
-            borderColor: 'rgb(255, 99, 132)',
-            backgroundColor: 'rgba(255, 99, 132, 0.5)',
-          },
-          {
-            label: 'Usuarios logeados',
-            data: dataLogeados,
-            borderColor: 'rgb(53, 162, 235)',
-            backgroundColor: 'rgba(53, 162, 235, 0.5)',
-          },
-          {
-            label: 'Compras',
-            data: dataCompras,
-            borderColor: 'rgb(132, 255, 99, 0.5)',
-            backgroundColor: 'rgba(132, 255, 99, 0.5)',
-          },
+            data: [
+              eval(dataVisitas.join('+')),
+              eval(dataLogeados.join('+')),
+              eval(dataCompras.join('+'))
+            ],
+            borderColor: [
+              'rgb(255, 99, 132)',
+              'rgb(132, 255, 99)',
+              'rgb(99, 132, 255)',
+            ],
+            backgroundColor: [
+              'rgb(255, 99, 132, 0.5)',
+              'rgb(132, 255, 99, 0.5)',
+              'rgb(99, 132, 255, 0.5)',
+            ],
+          }
         ],
     };
+    }else{
+      data = {
+          labels,
+          datasets: [
+            {
+              fill: mode?.split('|').includes('fill'),
+              label: 'Visitas',
+              data: dataVisitas,
+              borderColor: 'rgb(255, 99, 132)',
+              backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            },
+            {
+              fill: mode?.split('|').includes('fill'),
+              label: 'Usuarios logeados',
+              data: dataLogeados,
+              borderColor: 'rgb(53, 162, 235)',
+              backgroundColor: 'rgba(53, 162, 235, 0.5)',
+            },
+            {
+              fill: mode?.split('|').includes('fill'),
+              label: 'Compras',
+              data: dataCompras,
+              borderColor: 'rgb(132, 255, 99, 0.5)',
+              backgroundColor: 'rgba(132, 255, 99, 0.5)',
+            },
+          ],
+      }
+    }
     
-    return (<Line options={options} data={data} />)
+    return (<Chart type={$type} height={800} options={{...options, maintainAspectRatio: false }} data={data} />)
 }
 
 
--- src/routes/Stats.jsx
+++ src/routes/Stats.jsx
@@ -24,13 +24,17 @@ import { useNavigate, useParams } from 'react-router-dom';
 import { getResource } from '../tools/resourceRequest';
 import { Refresh } from '@mui/icons-material';
 import { Sell, Visits } from '.'
+import { ListView } from '../components'
 
 export default function Stats( props ) {
     const navigate = useNavigate()
     const [labels, setLabels] = useState([])
     const [start, setStart] = useState(null)
     const [end, setEnd] = useState(null)
-    const [stat, setStat] = useState(0)
+    const [stat, setStat] = useState('line')
+    const [dataVisitas, setdataVisitas] = useState([])
+    const [dataLogeados, setdataLogeados] = useState([])
+    const [dataCompras, setdataCompras] = useState([])
 
     ChartJS.register(
         CategoryScale,
@@ -70,11 +74,19 @@ export default function Stats( props ) {
             result.push(fechaInicio.getFullYear() + '-' + (fechaInicio.getMonth() + 1) + '-' + fechaInicio.getDate());
         }
         setLabels( prev => result )
+        const visitas = result.map(() => Math.floor((Math.random()*1000)))
+        const logeados = visitas.map( dato => Math.floor(Math.random()*dato ))
+        const compras = logeados.map( dato => Math.floor(Math.random()*dato/2 ))
+        setdataVisitas( prev => visitas )
+        setdataLogeados( prev => logeados )
+        setdataCompras( prev => compras )
     }        
 
     const handlerChangeDate = (handlerState) => (event) => {
         handlerState( prev => event.target.value )
     }
+
+    
     
 
     
@@ -91,9 +103,11 @@ export default function Stats( props ) {
                     <Typography style={{fontSize:'2rem', textAlign:'center', fontWeight:'bold'}}>Estadisticas</Typography>
                 </Grid>
                 <Grid item xs={2}>
-                    <Select label='Tipo' defaultValue={0} onChange={ev=>{setStat(ev.target.value)}}>
-                        <MenuItem value={0}>Ventas</MenuItem>
-                        <MenuItem value={1}>Usuarios</MenuItem>
+                    <Select label='Tipo' defaultValue={'line'} onChange={ev=>{setStat(ev.target.value)}}>
+                        <MenuItem value={'line'}>Line</MenuItem>
+                        <MenuItem value={'line.fill'}>Line (Fill)</MenuItem>
+                        <MenuItem value={'pie'}>Pie</MenuItem>
+                        <MenuItem value={'bar'}>Bar</MenuItem>
                     </Select>
                 </Grid>
                 <Grid item xs={2}>
@@ -108,8 +122,35 @@ export default function Stats( props ) {
                     </IconButton>
                 </Grid>
                 <Grid item xs={12}>
-                    { stat === 0 && <Sell labels={labels} /> }
-                    { stat === 1 && <Visits labels={labels}  /> }
+                    <Sell 
+                        type={stat} 
+                        labels={labels} 
+                        dataVisitas={dataVisitas}
+                        dataLogeados={dataLogeados}
+                        dataCompras={dataCompras}                    
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <ListView 
+                        headers={[
+                            { key: 'data', name: 'Fecha' },
+                            { key: 'visitas', name: 'Visitas', default: 0 },
+                            { key: 'usuarios', name: 'Logins', default: 0 },
+                            { key: 'compras', name: 'Compras', default: 0 },
+                        ]}
+                        disableSelection
+                        records={ labels.map( (label, index) => 
+                            ({
+                                data: label,
+                                visitas: dataVisitas[index],
+                                usuarios: dataLogeados[index],
+                                compras: dataCompras[index]
+                            })
+                        )}
+                        id={'id'}
+                    />
+                    
+                    
                 </Grid>
             </Grid>
         </Box>
